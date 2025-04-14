bc753f32910dc97bbe8d997848f579bb6a0f2b79
backack <backack2@gmail.com>
2025-04-14 22:36:02 +0900

    JPA와 H2 데이터베이스 연결 - JPA의 메서드 기반 쿼리

--- src/main/java/com/codezero/BookRental/BookRentalApplication.java
+++ src/main/java/com/codezero/BookRental/BookRentalApplication.java
@@ -22,6 +22,18 @@ public class BookRentalApplication {
 			bookRepository.save(new Book("Spring Boot 시작하기", "홍길동", LocalDate.of(2025, 3, 2)));
 			bookRepository.save(new Book("스프링부트 완전정복", "코드제로", LocalDate.of(2025, 4,7)));
 			bookRepository.save(new Book("스프링 고급", "홍길동", LocalDate.of(2025,3,1)));
+			System.out.println("[전체조회]");
+			bookRepository.findAll().forEach(book -> System.out.println(book.getTitle()));
+			System.out.println("[작가로 조회]");
+			bookRepository.findByAuthor("홍길동").forEach(book -> {
+				System.out.println(book.getTitle() + "/" + book.getAuthor() + "/" + book.getPublishedDate());
+			});
+			System.out.println("[작가와 출판일로 조회]");
+			bookRepository.findByAuthorAndPublishedDate("홍길동", LocalDate.of(2025, 3, 1)).forEach(
+					book-> {
+						System.out.println(book.getTitle() + "/" + book.getAuthor() + "/" + book.getPublishedDate());
+					}
+			);
 		};
 	}
 
--- src/main/java/com/codezero/BookRental/BookRepository.java
+++ src/main/java/com/codezero/BookRental/BookRepository.java
@@ -2,8 +2,12 @@ package com.codezero.BookRental;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByTitle(String title);
+    List<Book> findByAuthor(String author);
+    List<Book> findByPublishedDate(LocalDate publishedDate);
+    List<Book> findByAuthorAndPublishedDate(String author, LocalDate publishedDate);
 }
